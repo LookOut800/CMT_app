701218604d9656a2a2f110f1bbaee4d53ae55e12
Charles Talbot <charlietalbot@gmail.com>
2015-03-29 10:15:54 -0400

    token auth controllers, factories, and directives for users ans posts

---
+++ app/scripts/app.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name CmtApp
+ * @description
+ * # CmtApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('CmtApp', [
+  'ngAnimate',
+  'ngAria',
+  'ngCookies',
+  'ngMessages',
+  'ngResource',
+  'ngRoute',
+  'ngSanitize',
+  'ngTouch',
+  'MainController',
+  'MainDirective'
+]).run(function($rootScope, $http, $window, $location, AuthFactory, PostsFactory){
+  if(AuthFactory.isAuthenticated()){
+    var data = JSON.parse($window.localStorage.getItem('cmt-user'));
+    $http.defaults.headers.common.Authorization = 'Token token=' + data.token;
+  } else {
+    $location.path('/login');
+  }
+
+  $rootScope.$on('$routeChangeStart',function(event,next){
+    if(!AuthFactory.isAuthenticated()){
+      $location.path('/login');
+    } else {
+      PostsFactory.getPosts();
+    }
+  });
+});
+
